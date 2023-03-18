00eaa53778450b59a2ce3587ccd3e2f1cf69dcf6
Agata Wagner <agamuffin@gmail.com>
2023-03-18 16:41:09 +0100

    Finish the task

--- WagnerTask/Program.cs
+++ WagnerTask/Program.cs
@@ -1,15 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.ComponentModel;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main()
     {
-        //Declare variables and then initialize to zero
-        double x1, y1, x2, y2, x3, y3;
-
         Console.WriteLine("Coordinates of 3 points in 2-dimensional coordinate system in C#\r");
         Console.WriteLine("-----------------------------Agata Wagner-----------------------\n");
+
+        //Declare variables and then initialize to zero
+        double x1, y1, x2, y2, x3, y3;
 
         // Ask the user to type the coordinate x of dot A
         Console.WriteLine("Type the coordinate x of dot A and then press Enter");
@@ -35,7 +39,7 @@ class Program
         Console.WriteLine("Type the coordinate y of dot C and then press Enter");
         y3 = Convert.ToDouble(Console.ReadLine());
 
-
+        // Name sides of triangle
         Point a = new Point(x1, y1);    
         Point b = new Point(x2, y2);
         Point c = new Point(x3, y3);    
@@ -45,8 +49,11 @@ class Program
         Console.WriteLine("Side A: " + triangle.getSideA());
         Console.WriteLine("Side B: " + triangle.getSideB());
         Console.WriteLine("Side C: " + triangle.getSideC());
+        Console.WriteLine("Perimeter:" + triangle.getPerimeter());
+        Console.WriteLine("Parity numbers in range from 0 to triangle perimeter:");
+        Program.printEvenNumbers(triangle.getPerimeter());
 
-        if(!triangle.isTriangle())
+        if (!triangle.isTriangle())
         {
             Console.WriteLine("Given points doesnt represent triangle.");
             return;
@@ -70,11 +77,18 @@ class Program
             Console.WriteLine("This triangle is NOT right.");
         }
     }
+    static void printEvenNumbers(double perimeter)
+    {
+        for (int i = 0; i <= Math.Floor(perimeter); i=i+2) // i to znaczy od ilu będziemy liczyć czyli od 0 / i++ oznacza i + 1
+        {
+            Console.WriteLine(i);
+        }  
+    }
 }
 
 class Point
 {
-    private double x, y; 
+    private double x, y;
     
     public Point(double x, double y)
     {
@@ -108,12 +122,12 @@ class Triangle
     }
 
     public double getSideA() { return sidea; }
-    public double getSideB() {  return sideb; }
-    public double getSideC() {  return sidec; }
+    public double getSideB() { return sideb; }
+    public double getSideC() { return sidec; }
 
     public bool isTriangle()
     {
-        if(sidea >= sideb && sidea >= sidec)
+        if (sidea >= sideb && sidea >= sidec)
         {
             return Math.Abs(sidea - sideb - sidec) > precision;
         }
@@ -131,7 +145,7 @@ class Triangle
 
     public bool isEquiliteral()
     {
-        return sidea == sideb && sideb == sidec;        
+        return sidea == sideb && sideb == sidec;
     }
 
     public bool isIsosceles()
@@ -141,7 +155,7 @@ class Triangle
 
     public bool isRight()
     {
-        if(sidea >= sideb && sidea >= sidec)
+        if (sidea >= sideb && sidea >= sidec)
         {
             return Math.Pow(sidea, 2) - (Math.Pow(sideb, 2) + Math.Pow(sidec, 2)) <= precision;
         }
@@ -156,4 +170,10 @@ class Triangle
             return Math.Pow(sidec, 2) - (Math.Pow(sidea, 2) + Math.Pow(sideb, 2)) <= precision;
         }
     }
+
+    public double getPerimeter()
+    {
+        return this.sidea + this.sideb + this.sidec;
+    }
+   
 }
